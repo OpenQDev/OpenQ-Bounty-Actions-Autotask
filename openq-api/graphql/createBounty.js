79ac1e47754f52611af3765543b957312938b3ca
Christopher Stevers <christopher.stevers@protonmail.com>
2022-08-22 18:43:34 -0400

    finished merging upstream

--- openq-api/graphql/createBounty.js
+++ openq-api/graphql/createBounty.js
@@ -3,23 +3,17 @@ const CREATE_BOUNTY = `
 		$address: String!
 		$bountyId: String!
 		$organizationId: String!
-<<<<<<< HEAD
 		$type: String!
 		$category: String
-=======
 		$bountyType: String!
->>>>>>> b92d55c25182fa1af03ec7728d1c047ac39750c5
 	) {
 		createBounty(
 			address: $address
 			bountyId: $bountyId
 			organizationId: $organizationId
-<<<<<<< HEAD
 			type: $type
 			category: $category
-=======
 			type: $bountyType
->>>>>>> b92d55c25182fa1af03ec7728d1c047ac39750c5
 		) {
 			address
 			bountyId
