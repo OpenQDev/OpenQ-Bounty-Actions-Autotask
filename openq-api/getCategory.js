f55d3f7bc7ee03ab3fdc6a239cd26ce865bd7b45
FlacoJones <andrew@openq.dev>
2022-10-06 16:07:43 -0500

    removes optional unfolding

--- openq-api/getCategory.js
+++ openq-api/getCategory.js
@@ -1,9 +1,9 @@
 const getCategory = (labels) => {
-	console.log(labels)
-		if (labels?.includes("non-profit")) {
-			return "non-profit";
-		}
-		return undefined;
-	};	
+	console.log(labels);
+	if (labels && labels.includes("non-profit")) {
+		return "non-profit";
+	}
+	return undefined;
+};
 
 module.exports = getCategory;
