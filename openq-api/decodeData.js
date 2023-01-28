0171cbf994ba4444b2d0f216202771edd9f7a68b
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2023-01-28 00:33:40 +0000

    completed adding creating user to bounties

--- openq-api/decodeData.js
+++ openq-api/decodeData.js
@@ -2,19 +2,22 @@ const {ethers} = require('ethers');
 const decodeData = (encodedData, bountyType)=>{
 	console.log(bountyType);
 		let abiCoder = new ethers.utils.AbiCoder();
-const type = parseInt(bountyType);
-switch (type) {
-	case 0:
+const type=  ethers.utils.formatEther( bountyType )
+console.log(type);
+switch (bountyType.hex) {
+	case "0x00":
 	  {
-const  [, fundingGoalBountyParamsEncoded] = encodedData;
-	return abiCoder.decode(
+const  fundingGoalBountyParamsEncoded = encodedData;
+const decodedData= abiCoder.decode(
 		  ['bool', 'address', 'uint256', 'bool', 'bool', 'bool', 'string', 'string', 'string'],
 		  fundingGoalBountyParamsEncoded
 		);
+		const issuerExternalUserId = decodedData[6];
+		return issuerExternalUserId
 	  }
-	case 1:{
-const  [, fundingGoalBountyParamsEncoded] = encodedData;
-	return abiCoder.decode(
+	case "0x01":{
+const   fundingGoalBountyParamsEncoded= encodedData;
+const decodedData= abiCoder.decode(
 		[	
 			'address',
 			'uint256',
@@ -30,24 +33,32 @@ const  [, fundingGoalBountyParamsEncoded] = encodedData;
 		  ],
 		  fundingGoalBountyParamsEncoded
 		);
+		
+		const issuerExternalUserId = decodedData[8];
+		  return issuerExternalUserId
 	  }
-	case 2:{
-		const  [, fundingGoalBountyParamsEncoded] = encodedData;
-			return abiCoder.decode(
+	case "0x02":{
+		const  fundingGoalBountyParamsEncoded = encodedData;
+		const decodedData =abiCoder.decode(
 				[	
 					'uint256[]', 'bool', 'address', 'uint256', 'bool', 'bool', 'bool', 'string', 'string', 'string'
 				  ],
 				  fundingGoalBountyParamsEncoded
 				);
+				
+				const issuerExternalUserId = decodedData[7];
+				  return issuerExternalUserId
 			  }
 	 
-	case 3:
+	case "0x03":
 	  {
-		const  [, fundingGoalBountyParamsEncoded] = encodedData;
-			return abiCoder.decode(
+		const   fundingGoalBountyParamsEncoded= encodedData;
+			const decodedData =  abiCoder.decode(
 				['uint256[]', 'address', 'bool', 'bool', 'bool', 'string', 'string', 'string'],
 				  fundingGoalBountyParamsEncoded
 				);
+				const issuerExternalUserId = decodedData[5];
+				  return issuerExternalUserId
 	  }
 	 }
 	}
