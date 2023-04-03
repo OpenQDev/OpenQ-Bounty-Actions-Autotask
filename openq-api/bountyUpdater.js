4194fa4cd92869bb904febbd8514cffd9c0280ff
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2023-04-03 14:24:47 +0000

    added updating of request state in api

--- openq-api/bountyUpdater.js
+++ openq-api/bountyUpdater.js
@@ -1,114 +1,167 @@
-const axios = require('axios');
-const getValueFromBalanceImpl = require('./getValueFromBalance.js');
-const createNewBountyImpl = require('./createNewBounty');
-const getIssueImpl = require('./getIssue');
-const getTokenReq = require('./getTokenReq');
-const updateBountyValuationImpl = require('./updateBountyValuation');
+const axios = require("axios");
+const getValueFromBalanceImpl = require("./getValueFromBalance.js");
+const createNewBountyImpl = require("./createNewBounty");
+const getIssueImpl = require("./getIssue");
+const getTokenReq = require("./getTokenReq");
+const updateBountyValuationImpl = require("./updateBountyValuation");
 const getCategory = require("./getCategory");
 const decodeData = require("./decodeData");
+const getSubgraphBounty = require("./getSubgraphBounty");
+const updateRequestStateToClosed = require("./updateRequestToClosed");
+const getUser = require("./getUser");
+const decodeSupportingDocsData = require("./decodeSupportingDocsData");
 const { ethers } = require("ethers");
+const getBountyTypeFromHexOrString = require("./utils/getBountyTypeFromHexOrString.js");
 
 /**
- * bountyUpdater takes in an event type and responds by creating a new Bounty document in OpenQ-API if it is a 
+ * bountyUpdater takes in an event type and responds by creating a new Bounty document in OpenQ-API if it is a
  * BountyCreated event, or triggering an update of the bounty's TVL if it is a TokenDepositReceived or DepositRefunded event
- * @param {The type of event} eventType 
- * @param { Staging or Production } baseUrl 
- * @param { Shared secret between this backend service and the OpenQ-API } openqApiSecret 
- * @param { Depending on event, will be different } params 
- * @returns 
+ * @param {The type of event} eventType
+ * @param { Staging or Production } baseUrl
+ * @param { Shared secret between this backend service and the OpenQ-API } openqApiSecret
+ * @param { Depending on event, will be different } params
+ * @returns
  */
 const bountyUpdater = async (
-	eventType,
-	baseUrl,
-	openqApiSecret,
-	params,
-	pat,
-	invoiceUrl,
-	coinApiUrl,
-	getIssue = getIssueImpl,
-	createNewBounty = createNewBountyImpl,
-	updateBountyValuation = updateBountyValuationImpl,
-	getValueFromBalance = getValueFromBalanceImpl,
-
+  eventType,
+  baseUrl,
+  openqApiSecret,
+  params,
+  pat,
+  invoiceUrl,
+  coinApiUrl,
+  getIssue = getIssueImpl,
+  createNewBounty = createNewBountyImpl,
+  updateBountyValuation = updateBountyValuationImpl,
+  getValueFromBalance = getValueFromBalanceImpl
 ) => {
-	return new Promise(async (resolve, reject) => {
-		try {
-			let result = null;
-			switch (eventType) {
-				case 'BountyCreated': {
-					const { bountyAddress, bountyId, organization, bountyType, data } = params;
-					const minterUuid = decodeData(data, bountyType);
-					const type = ethers.BigNumber.from(bountyType).toString();
-					let category;
+  return new Promise(async (resolve, reject) => {
+    try {
+      let result = null;
+      switch (eventType) {
+        case "BountyCreated": {
+          const { bountyAddress, bountyId, organization, bountyType, data } =
+            params;
+          const minterUuid = decodeData(data, bountyType);
+          const type = ethers.BigNumber.from(bountyType).toString();
+          let category;
 
-					let bountyRepositoryId;
-					try {
-						const { labels, repositoryId } = await getIssue(bountyId, pat);
-						bountyRepositoryId = repositoryId;
-						category = getCategory(labels);
-					} catch (err) {
-						console.log(err);
-					}
+          let bountyRepositoryId;
+          try {
+            const { labels, repositoryId } = await getIssue(bountyId, pat);
+            bountyRepositoryId = repositoryId;
+            category = getCategory(labels);
+          } catch (err) {
+            console.log(err);
+          }
 
-					try {
-						result = await createNewBounty(baseUrl, openqApiSecret, bountyAddress, bountyId, organization, bountyRepositoryId, category, type, minterUuid);
-					} catch (error) {
-						console.error('error creating new bounty', JSON.stringify(error));
-						reject(new Error('ERROR CREATING NEW BOUNTY'));
-					}
+          try {
+            result = await createNewBounty(
+              baseUrl,
+              openqApiSecret,
+              bountyAddress,
+              bountyId,
+              organization,
+              bountyRepositoryId,
+              category,
+              type,
+              minterUuid
+            );
+          } catch (error) {
+            console.error("error creating new bounty", JSON.stringify(error));
+            reject(new Error("ERROR CREATING NEW BOUNTY"));
+          }
 
-				
-					return resolve(result);
-				}
-				case 'TokenDepositReceived': {
-					const { volume, tokenAddress, bountyAddress } = params;
-					try {
+          return resolve(result);
+        }
+        case "TokenDepositReceived": {
+          const { volume, tokenAddress, bountyAddress } = params;
+          try {
+            const tokenReq = getTokenReq(tokenAddress, volume);
+            const total = await getValueFromBalance(tokenReq, coinApiUrl);
+            result = await updateBountyValuation(baseUrl, openqApiSecret, {
+              tvc: 0,
+              tvl: total,
+              address: bountyAddress,
+            });
+          } catch (error) {
+            console.error(
+              "error funding bounty",
+              JSON.stringify(error),
+              bountyAddress,
+              tokenAddress
+            );
+            //	reject(new Error('ERROR UPDATING BOUNTY'));
+          }
+          return resolve(result);
+        }
+        case "DepositRefunded": {
+          const { bountyAddress, tokenAddress, volume } = params;
+          try {
+            const tokenReq = getTokenReq(tokenAddress, volume);
+            const total = await getValueFromBalance(tokenReq, coinApiUrl);
+            result = await updateBountyValuation(baseUrl, openqApiSecret, {
+              tvc: 0,
+              tvl: total * -1,
+              address: bountyAddress,
+            });
+          } catch (error) {
+            console.error("ERROR UPDATING BOUNTY", JSON.stringify(error));
+            reject(new Error("Unknown Event"));
+          }
+          return resolve(result);
+        }
+        case "TokenBalanceClaimed": {
+          const { bountyAddress, tokenAddress, volume } = params;
 
-						const tokenReq = getTokenReq(tokenAddress, volume);
-						const total = await getValueFromBalance(tokenReq, coinApiUrl);
-						result = await updateBountyValuation(baseUrl, openqApiSecret, {tvc:0,  tvl: total, address: bountyAddress, });
-					} catch (error) {
-						console.error('error funding bounty', JSON.stringify(error), bountyAddress, tokenAddress);
-						//	reject(new Error('ERROR UPDATING BOUNTY'));
-					}
-					return resolve(result);
-				}
-				case 'DepositRefunded': {
-					const { bountyAddress, tokenAddress, volume } = params;
-					try {
+          try {
+            const tokenReq = getTokenReq(tokenAddress, volume);
+            const total = await getValueFromBalance(tokenReq, coinApiUrl);
+            result = await updateBountyValuation(baseUrl, openqApiSecret, {
+              tvc: total,
+              tvl: 0,
+              address: bountyAddress,
+            });
+          } catch (error) {
+            console.log(error);
+          }
+          return resolve(result);
+        }
+        case "BountyClosed": {
+          return resolve({});
+        }
+        case "SupportingDocumentsCompleteSet": {
+          const [tier, bool] = decodeSupportingDocsData(
+            params.data,
+            params.bountyType
+          );
 
-						const tokenReq = getTokenReq(tokenAddress, volume);
-						const total = await getValueFromBalance(tokenReq, coinApiUrl);
-						result = await updateBountyValuation(baseUrl, openqApiSecret, {tvc:0,  tvl: total * -1, address: bountyAddress, });
-					} catch (error) {
-						console.error('ERROR UPDATING BOUNTY', JSON.stringify(error));
-						reject(new Error('Unknown Event'));
-					}
-					return resolve(result);
-				}
-				case 'TokenBalanceClaimed': {
-					const { bountyAddress, tokenAddress, volume } = params;
-					
-					try {
-						const tokenReq = getTokenReq(tokenAddress, volume);
-						const total = await getValueFromBalance(tokenReq, coinApiUrl);
-						result = await updateBountyValuation(baseUrl, openqApiSecret, { tvc: total, tvl: 0, address: bountyAddress, });
-					} catch (error) {
-						console.log(error);
-					}
-					return resolve(result);
-				}
-				case 'BountyClosed': {
-					return resolve({});
-				}
-				default: {
-					reject(new Error(`Unknown Event: ${eventType}`));
-				}
-			}
-		} catch (error) {
-			reject(error);
-		}
-	});
+          const { data: bountyData } = await getSubgraphBounty(
+            params.bountyAddress.toLowerCase()
+          );
+          const tierInt = parseInt(getBountyTypeFromHexOrString(tier));
+          const winner = bountyData.bounty.tierWinners?.[tierInt];
+          // rn I know what tier and what user the request is for.
+          const apiUser = await getUser(baseUrl, openqApiSecret, winner);
+          const requestingUserId = apiUser.data.user.id;
+          const bountyAddress = params.bountyAddress;
+          const state = bool ? "CLOSED" : "OPEN";
+          const result = await updateRequestStateToClosed(
+            baseUrl,
+            openqApiSecret,
+            { requestingUserId, bountyAddress, state }
+          );
+          resolve(result);
+        }
+
+        default: {
+          resolve({});
+        }
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
 module.exports = bountyUpdater;
