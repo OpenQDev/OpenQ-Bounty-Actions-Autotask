58e758e5d62d7f47d3d092fbd16902fd930748d4
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2023-04-26 14:44:21 +0000

    added error handling to getTokenReq

--- openq-api/getTokenReq.js
+++ openq-api/getTokenReq.js
@@ -1,39 +1,41 @@
+const openqIndexableTokens = require("../constants/openq-polygon-mainnet-indexable.json");
+const { ethers } = require("ethers");
 
-const openqIndexableTokens = require('../constants/openq-polygon-mainnet-indexable.json');
-const  { ethers } = require('ethers' );
-
-  
-  function getTokenReq(address, volume) {
+const getTokenReq = (address, volume) => {
   const formattedVolume = ethers.utils.formatUnits(volume, 0);
-    const checkSummedAddress = ethers.utils.getAddress(address);
-    const lowerCaseAddress = address.toLowerCase();
- let token={}
-    if (openqIndexableTokens[checkSummedAddress]) {
-      token =  openqIndexableTokens[checkSummedAddress];
-    } else if (openqIndexableTokens[lowerCaseAddress]) {
-      token =  openqIndexableTokens[lowerCaseAddress];
-    }
-    else{
-    token =  {
+  const checkSummedAddress = ethers.utils.getAddress(address);
+  const lowerCaseAddress = address.toLowerCase();
+  let token = {};
+  if (openqIndexableTokens[checkSummedAddress]) {
+    token = openqIndexableTokens[checkSummedAddress];
+  } else if (openqIndexableTokens[lowerCaseAddress]) {
+    token = openqIndexableTokens[lowerCaseAddress];
+  } else {
+    token = {
       chainId: 137,
       name: `${address.substring(0, 5)}
 			...
 			${address.substring(39)}`,
-      symbol: 'CUSTOM',
+      symbol: "CUSTOM",
       decimals: 18,
       address: checkSummedAddress,
-      path: '/crypto-logos/ERC20.svg',
+      path: "/crypto-logos/ERC20.svg",
     };
-    }
-	const tokenAddress = token.address;
-	const returnToken=  {
-    network: 'polygon-pos',
-		tokenVolumes: {
-			[tokenAddress]: {
-      volume: formattedVolume,
-			decimals: token.decimals},
-		}
-	};
-  return returnToken;
   }
-  module.exports = getTokenReq;
+  try {
+    const tokenAddress = token.address;
+    const returnToken = {
+      network: "polygon-pos",
+      tokenVolumes: {
+        [tokenAddress]: {
+          volume: formattedVolume,
+          decimals: token.decimals,
+        },
+      },
+    };
+    return returnToken;
+  } catch (err) {
+    return {};
+  }
+};
+module.exports = getTokenReq;
